6f32d005221dcb6dd87a1576d1b2727f8504b83c
ihsandroid1 <maenbos30@gmail.com>
2020-05-20 01:54:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,63 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>Welcome to My World!</h1>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Silakan Pilih <strong>Tombol</strong> dibawah ini:
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/ihsandroid"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <p>Linkedin</p>
         </a>
+
+        <a
+          className="App-link"
+          href="https://github.com/hahamen-ihs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <p>Github</p>
+        </a>
+
+        <a
+          className="App-link"
+          href="https://www.ihsanmagazine.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <p>Website</p>
+        </a>
+
+        <a
+          className="App-link"
+          href="https://www.youtube.com/c/ihsanmagazine"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        <p>Youtube</p>
+        </a>
+
+    
+
       </header>
+
+    <footer>
+      <p>With Love By Ihsandroid</p>
+    </footer>
+
     </div>
   );
 }
